a44f41b897b3efdd5963ff4d584826c4876ed61b
TEEPARTHILSSNAGAVALLI <99274565+TEEPARTHILSSNAGAVALLI@users.noreply.github.com>
2022-05-20 08:34:36 +0530

    Create in_love_with_primes.py

---
+++ in_love_with_primes.py
@@ -0,0 +1,10 @@
+tc = int(input())
+for i in range(tc):
+    e = int(input())
+    if e%2==0:
+        if e > 2:
+                print("Deepa")
+        else:
+                print("Arjit")
+    else:
+            print("Arjit")    
