fcb9893e13d0667322e3b8c6c5b7564698c7aea6
TEEPARTHILSSNAGAVALLI <99274565+TEEPARTHILSSNAGAVALLI@users.noreply.github.com>
2022-05-20 22:22:42 +0530

    Create bitwiseoperations.py

---
+++ bitwiseoperations.py
@@ -0,0 +1,16 @@
+a = int(input("Enter 'a' value:"))
+b = int(input("Enter 'b' value:"))
+c = a&b
+print("bitwise & operation of a and b is:",c)
+c = a^b
+print("bitwise ^ operation of a and b is:",c)
+c = a|b
+print("bitwise | operation of a and b is:",c)
+c = a<<b
+print("bitwise << operation of a and b is:",c)
+c = a>>b
+print("bitwise >> operation of a and b is:",c)
+c = ~a
+print("bitwise ~ operation of a is:",c)
+c = ~b
+print("bitwise ~ operation of b is:",c)
